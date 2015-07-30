1d4a4e0576b36fbe08b0521d25776629464aad5d
ahu8867 <dnrguddks@naver.com>
2015-07-30 10:28:58 +0900

    completed

---
+++ testproject/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace testproject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PaperPlane = new System.Windows.Forms.PictureBox();
+            this.BrickRabel = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.RecordLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PaperPlane)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gulim", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(37, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "난이도";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.PaperPlane);
+            this.panel1.Controls.Add(this.BrickRabel);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(12, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(649, 462);
+            this.panel1.TabIndex = 5;
+            // 
+            // PaperPlane
+            // 
+            this.PaperPlane.Image = global::testproject.Properties.Resources.Paper_airplane;
+            this.PaperPlane.Location = new System.Drawing.Point(72, 160);
+            this.PaperPlane.Name = "PaperPlane";
+            this.PaperPlane.Size = new System.Drawing.Size(64, 64);
+            this.PaperPlane.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.PaperPlane.TabIndex = 0;
+            this.PaperPlane.TabStop = false;
+            this.PaperPlane.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // BrickRabel
+            // 
+            this.BrickRabel.Location = new System.Drawing.Point(631, 67);
+            this.BrickRabel.Margin = new System.Windows.Forms.Padding(3);
+            this.BrickRabel.Name = "BrickRabel";
+            this.BrickRabel.Size = new System.Drawing.Size(20, 157);
+            this.BrickRabel.TabIndex = 2;
+            this.BrickRabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::testproject.Properties.Resources._7;
+            this.pictureBox2.Location = new System.Drawing.Point(631, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 462);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(669, 155);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(139, 230);
+            this.panel2.TabIndex = 9;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.RecordLabel);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(669, 10);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(139, 139);
+            this.panel3.TabIndex = 10;
+            // 
+            // RecordLabel
+            // 
+            this.RecordLabel.Font = new System.Drawing.Font("Gulim", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.RecordLabel.Location = new System.Drawing.Point(3, 57);
+            this.RecordLabel.Name = "RecordLabel";
+            this.RecordLabel.Size = new System.Drawing.Size(133, 49);
+            this.RecordLabel.TabIndex = 1;
+            this.RecordLabel.Text = "0";
+            this.RecordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gulim", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(37, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "점수판";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Gulim", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(669, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 71);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Game Start";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 484);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PaperPlane)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PaperPlane;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label BrickRabel;
+        private System.Windows.Forms.Label RecordLabel;
+    }
+}
+
---
+++ testproject/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace testproject
+{
+    public partial class Form1 : Form
+    {
+        WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+        int Record = 0;
+        Timer timer = new System.Windows.Forms.Timer();
+        protected override bool IsInputKey(Keys keydata)
+        {
+            switch (keydata)
+            {
+                case Keys.Enter:
+                    int x = PaperPlane.Location.X;
+                    int y = PaperPlane.Location.Y;
+                    y -= 50;
+                    PaperPlane.Location = new Point(x, y);
+                    return true;
+                case Keys.Shift | Keys.Enter:
+                    return true;
+            }
+            return base.IsInputKey(keydata);
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            int borderWidth = 5;
+            Color borderColor = Color.Pink;
+            ControlPaint.DrawBorder(e.Graphics, e.ClipRectangle, borderColor,
+             borderWidth, ButtonBorderStyle.Solid, borderColor, borderWidth,
+            ButtonBorderStyle.Solid, borderColor, borderWidth,
+             ButtonBorderStyle.Solid,
+            borderColor, borderWidth, ButtonBorderStyle.Solid);
+             
+        }
+
+        public Form1()
+        { 
+            InitializeComponent();
+            player.URL = "43.mp3";
+            player.controls.stop();
+            panel1.BackColor = Color.AliceBlue;
+            panel2.BackColor = Color.SkyBlue ;
+            panel3.BackColor = Color.YellowGreen;
+            this.BackColor = System.Drawing.Color.White;
+        }
+        
+
+        void Form1_KeyDown(object sender, KeyEventArgs e)
+        { 
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        { 
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            RecordLabel.Text = Convert.ToString(Record);
+            Record += 1;
+            int random_brick_Y = r.Next(15, 157);
+            int X_location = pictureBox2.Location.X;
+            int X_brick = BrickRabel.Location.X;
+            int Y_brick = BrickRabel.Location.Y;
+            int brick = random_brick_Y;
+
+            PaperPlane.Location = new Point(PaperPlane.Location.X, PaperPlane.Location.Y + 5);
+            pictureBox2.Location = new Point(X_location - 10, pictureBox2.Location.Y);
+            BrickRabel.Location = new Point(X_brick - 10, BrickRabel.Location.Y);
+            if (X_location < 0 && X_brick < 0)
+            {
+                X_location += 600;
+                X_brick += 604;
+                pictureBox2.Location = new Point(X_location - 1, pictureBox2.Location.Y);
+                BrickRabel.Location = new Point(X_brick - 5, random_brick_Y);
+            }
+            if (pictureBox2.Location.X < 133 && pictureBox2.Location.X > 71)
+            {
+                if (PaperPlane.Location.Y >= BrickRabel.Location.Y + 70 || PaperPlane.Location.Y <= BrickRabel.Location.Y)
+                {
+                    timer.Stop();
+                    if(MessageBox.Show("게임을 다시 시작하시겠습니까?","Game Over", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        Application.Restart();
+                    }
+                    else
+                    {
+                        Application.Exit();
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1_Tick(sender, e);
+            timer.Interval = 30;
+            timer.Tick += new EventHandler(timer1_Tick);
+            timer.Start();
+            player.controls.play();
+            button1.Enabled = false;
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
